143a8a281d9bc687edb33e0e7dc761861c242f38
amorx1 <akshay.morx@gmail.com>
2021-07-26 18:46:13 +1200

    Added image processing

--- Source/Interpolator.py
+++ Source/Interpolator.py
@@ -7,6 +7,8 @@ from matplotlib.tri import Triangulation
 import numpy as np
 import os
 from utilities import parse_args, numericalSort
+import io
+import cv2
 
 # returns triangulation object
 def triangulate(mesh):
@@ -34,39 +36,27 @@ def render(triangulation, xi, yi, C, it, interpolator:str, smoothing_algo:str=No
     plt.set_cmap('plasma')
     plt.savefig('_'+str(it)+'.png')
 
-    # geom_interpolation = CubicTriInterpolator(triangulation, C, kind="geom")
-    # min_e_interpolation = CubicTriInterpolator(triangulation, C, kind="min_E")
-    # linear_interpolation = LinearTriInterpolator(triangulation, C)
+    print("Frame " + str(it) + ": done")
 
-    # geom_res = geom_interpolation(xi, yi)
-    # min_e_res = min_e_interpolation(xi, yi)
-    # linear_res = linear_interpolation(xi, yi)
+def render_array(triangulation, xi, yi, C, it, interpolator:str, smoothing_algo:str=None):
 
-    # fig, axs = plt.subplots(nrows=2, ncols=2)
-    # axs = axs.flatten()
+    if interpolator == "linear": interpolation = LinearTriInterpolator(triangulation, C)
 
-    # Triangulation
-    # plt.tricontour(triangulation, C)
-    # plt.triplot(triangulation, 'ko-')
-
-    # axs[1].contourf(xi, yi, geom_res)
-    # # axs[1].plot(xi, yi, 'k-', lw=0.5, alpha=0.1)
-    # # axs[1].plot(xi.T, yi.T, 'k-', lw=0.5, alpha=0.1)
-    # axs[1].set_title("Cubic interpolation,\nkind='geom")
-
-    # plt.contourf(xi, yi, min_e_res, vmax=1, vmin=0)
-    # plt.plot(xi, yi, 'k-', lw=0.5, alpha=0.1)
-    # plt.plot(xi.T, yi.T, 'k-', lw=0.5, alpha=0.1)
-    # plt.title = "Cubic interpolation,\nkind='min_E'"
-    # plt.set_cmap('plasma')
-    # plt.show()
-    # plt.savefig('_'+str(it)+'.png')
-
-    # plt.contourf(xi, yi, linear_res, vmax=1, vmin=0)
-    # # # plt.plot(xi, yi, 'k-', lw=0.5, alpha=0.5)
-    # # # plt.plot(xi.T, yi.T, 'k-', lw=0.5, alpha=0.5)
-    # plt.set_cmap('plasma')
-    # plt.savefig('_'+str(it)+'.png')
+    if smoothing_algo == "geom": smoothing = "geom"
+    if smoothing_algo == "min_E": smoothing = "min_E"
+    if interpolator == "cubic": interpolation = CubicTriInterpolator(triangulation, C, kind=smoothing)
+    
+    res = interpolation(xi, yi,)
+    fig = plt.contourf(xi, yi, res, vmax=1, vmin=0)
+    fig.set_cmap('plasma')
+    
+    with io.BytesIO() as buff:
+        fig.savefig(buff, format='raw')
+        buff.seek(0)
+        data = np.frombuffer(buff.getvalue(), dtype=np.uint8)
+    w, h = fig.canvas.get_width_height()
+    im = data.reshape((int(h), int(w), -1))
+    cv2.imwrite(("_"+str(it)+".tiff"), cv2.GaussianBlur(im, (10,10), 0))
 
     print("Frame " + str(it) + ": done")
 
@@ -89,6 +79,27 @@ def generate_frames(fNames, interpolator:str, smoothing:str=None):
         render(triangulation, xi, yi, C, it, settings["interpolator"], settings["smoothing"])
         it += 1
 
+def generate_frame_arrays(fNames, interpolator:str, smoothing:str=None):
+
+    settings = {
+        "interpolator": interpolator,
+        "smoothing": smoothing,
+    }
+
+    sampling_mesh = meshio.read(fNames[1])
+    x, y = get_coords(sampling_mesh)
+    xi, yi = np.meshgrid(np.linspace(min(x),max(x),num=1000),np.linspace(min(y),max(y),num=1000))
+    triangulation = triangulate(sampling_mesh)
+    it = 0
+
+    for file in fNames:
+        C = meshio.read(file).point_data["Con"]
+        render_array(triangulation, xi, yi, C, it, settings["interpolator"], settings["smoothing"])
+        it += 1
+
+def process():
+    return
+
 def main():
     try:
         os.chdir("DATA")    # FOR TESTING ONLY
@@ -99,7 +110,7 @@ def main():
 
     if fNames:
         try:
-            generate_frames(fNames, "linear")
+            generate_frame_arrays(fNames, "linear")
 
         except:
             raise RuntimeError("There was a problem rendering the results")

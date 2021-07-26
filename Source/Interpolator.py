0239ba9addd9daa9920ae0c8ab88032354180aac
amorx1 <akshay.morx@gmail.com>
2021-07-26 18:16:16 +1200

    refactored rendering

--- Source/Interpolator.py
+++ Source/Interpolator.py
@@ -20,19 +20,26 @@ def get_coords(mesh):
     y = mesh.points[:,1]
     return x, y
 
-# returns concentration from each file (not constant)
-def get_Conc(mesh):
-    C = mesh.point_data["Con"]
-    return C
+# interpolator
+def render(triangulation, xi, yi, C, it, interpolator:str, smoothing_algo:str=None):
 
-def render(triangulation, xi, yi, C, it):
+    if interpolator == "linear": interpolation = LinearTriInterpolator(triangulation, C)
+
+    if smoothing_algo == "geom": smoothing = "geom"
+    if smoothing_algo == "min_E": smoothing = "min_E"
+    if interpolator == "cubic": interpolation = CubicTriInterpolator(triangulation, C, kind=smoothing)
+    
+    res = interpolation(xi, yi,)
+    plt.contourf(xi, yi, res, vmax=1, vmin=0)
+    plt.set_cmap('plasma')
+    plt.savefig('_'+str(it)+'.png')
 
     # geom_interpolation = CubicTriInterpolator(triangulation, C, kind="geom")
-    min_e_interpolation = CubicTriInterpolator(triangulation, C, kind="min_E")
+    # min_e_interpolation = CubicTriInterpolator(triangulation, C, kind="min_E")
     # linear_interpolation = LinearTriInterpolator(triangulation, C)
 
     # geom_res = geom_interpolation(xi, yi)
-    min_e_res = min_e_interpolation(xi, yi)
+    # min_e_res = min_e_interpolation(xi, yi)
     # linear_res = linear_interpolation(xi, yi)
 
     # fig, axs = plt.subplots(nrows=2, ncols=2)
@@ -47,37 +54,55 @@ def render(triangulation, xi, yi, C, it):
     # # axs[1].plot(xi.T, yi.T, 'k-', lw=0.5, alpha=0.1)
     # axs[1].set_title("Cubic interpolation,\nkind='geom")
 
-    plt.contourf(xi, yi, min_e_res, vmax=1, vmin=0)
+    # plt.contourf(xi, yi, min_e_res, vmax=1, vmin=0)
     # plt.plot(xi, yi, 'k-', lw=0.5, alpha=0.1)
     # plt.plot(xi.T, yi.T, 'k-', lw=0.5, alpha=0.1)
     # plt.title = "Cubic interpolation,\nkind='min_E'"
-    plt.set_cmap('plasma')
-    plt.savefig('_'+str(it)+'.png')
+    # plt.set_cmap('plasma')
+    # plt.show()
+    # plt.savefig('_'+str(it)+'.png')
 
-    # axs[3].contourf(xi, yi, linear_res)
-    # # axs[1].plot(xi, yi, 'k-', lw=0.5, alpha=0.5)
-    # # axs[1].plot(xi.T, yi.T, 'k-', lw=0.5, alpha=0.5)
-    # axs[3].set_title("Linear interpolation")
+    # plt.contourf(xi, yi, linear_res, vmax=1, vmin=0)
+    # # # plt.plot(xi, yi, 'k-', lw=0.5, alpha=0.5)
+    # # # plt.plot(xi.T, yi.T, 'k-', lw=0.5, alpha=0.5)
+    # plt.set_cmap('plasma')
+    # plt.savefig('_'+str(it)+'.png')
 
-    print("Done")
+    print("Frame " + str(it) + ": done")
+
+# generate frames from data
+def generate_frames(fNames, interpolator:str, smoothing:str=None):
+
+    settings = {
+        "interpolator": interpolator,
+        "smoothing": smoothing,
+    }
+
+    sampling_mesh = meshio.read(fNames[1])
+    x, y = get_coords(sampling_mesh)
+    xi, yi = np.meshgrid(np.linspace(min(x),max(x),num=1000),np.linspace(min(y),max(y),num=1000))
+    triangulation = triangulate(sampling_mesh)
+    it = 0
+
+    for file in fNames:
+        C = meshio.read(file).point_data["Con"]
+        render(triangulation, xi, yi, C, it, settings["interpolator"], settings["smoothing"])
+        it += 1
 
 def main():
     try:
         os.chdir("DATA")    # FOR TESTING ONLY
     except:
-        print("Invalid directory")
+        raise IOError("Invalid directory")
 
     fNames = parse_args()["fileNames"]
-    sampling_mesh = meshio.read(fNames[1])
-    x, y = get_coords(sampling_mesh)
-    xi, yi = np.meshgrid(np.linspace(min(x),max(x),num=1000),np.linspace(min(y),max(y),num=1000))
-    triangulation = triangulate(sampling_mesh)
-    
-    it = 0
-    for file in fNames:
-        C = meshio.read(file).point_data["Con"]
-        render(triangulation, xi, yi, C, it)
-        it += 1
+
+    if fNames:
+        try:
+            generate_frames(fNames, "linear")
+
+        except:
+            raise RuntimeError("There was a problem rendering the results")
 
 if __name__ == "__main__":
     main()

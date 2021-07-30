05678670b8a5eda4bc60190e4b2635d9c8776015
amorx1 <akshay.morx@gmail.com>
2021-07-31 01:28:23 +1200

    refactored to more objects

--- Source/Pipeline.py
+++ Source/Pipeline.py
@@ -3,6 +3,7 @@
 """
 
 from re import X
+import tensorflow as tf
 from typing import IO
 from matplotlib.tri.triinterpolate import LinearTriInterpolator
 import meshio
@@ -14,21 +15,30 @@ from matplotlib.tri import Triangulation
 import numpy as np
 import pandas as pd
 import os
-import io
-import cv2
-from utilities import getFiles, numericalSort, relative_error
+import sys
+import time
+from Objects import *
+from utilities import getFiles, numericalSort, relative_error, neural_net, mean_squared_error, Navier_Stokes_2D, tf_session
 
 class Pipeline:
 
     def __init__(self, fileNames = None, mesh:dict = None, triangulation = None, inputData:dict = None,
-                         predictions: dict=None, xi:np.ndarray = None, yi:np.ndarray = None):
+                         predictions: dict=None, xi:np.ndarray = None, yi:np.ndarray = None, HFM = None):
 
+        self.HFM = None
         self.fileNames = fileNames
         self.mesh = mesh
         self.triangulation = triangulation
         self.mesh_temp = None
         self.xi = None
         self.yi = None
+        self.T = None
+        self.N = None
+        self.Inputs = InputData()
+        self.TrainingData = TrainingData()
+        self.TestData = TestData()
+        self.Equations = Equations()
+        self.Predictions = Predictions()
         self.input_data = {
             "x_star": None,
             "y_star": None,
@@ -38,11 +48,32 @@ class Pipeline:
             "V_star": None,
             "P_star": None
         }
+        # InputData()
+        self.rearranged_inputs = {
+            "X_star": None,
+            "Y_star": None,
+            "T_star": None,
+            "C_star": None,
+            "U_star": None,
+            "V_star": None,
+            "P_star": None
+        }
+        self.eqns = {
+            "t_eqns": None,
+            "x_eqns": None,
+            "y_eqns": None
+        }
         self.predictions = {
             "C_pred": None,
             "U_pred": None,
             "V_pred": None,
             "P_pred": None
+        }
+        self.train_data = {
+            "t_data": None,
+            "x_data": None,
+            "y_data": None,
+            "c_data": None,
         }
         self.test_data = {
             "x_test": None,
@@ -64,9 +95,22 @@ class Pipeline:
             "smoothing_algorithm": None
         }
 
+    def setTN(self):
+        self.T = self.Inputs.t_star.shape[0]
+        self.N = self.Inputs.x_star.shape[0]
 
+    # HFM object instantiated with input data filled
+    def model(self):
+        if self.HFM == None:
+            if (self.input_data != None):
+                self.HFM = HFM(t_data=self.rearranged_inputs["t_star"], x_data=self.rearranged_inputs["x_star"], y_data=self.rearranged_inputs["y_star"], c_data=self.rearranged_inputs["C_star"], t_eqns=self.eqns["t_eqns"], x_eqns=self.eqns["x_eqns"], y_eqns=self.eqns["y_eqns"], layers=self.layers, batch_size=self.batch_size, Pec=self.Pec, Rey=self.Rey)
+            else:
+                raise IOError("HFM cannot be created without input data!")
+        else:
+            raise IOError("An HFM object already exists within the Pipeline!")
     # get data for training from vtu files
     def extractData(self, fileNames):
+
         # read files
         x_star = pd.DataFrame().astype(np.float)
         y_star = pd.DataFrame().astype(np.float)
@@ -127,15 +171,33 @@ class Pipeline:
                     print("There are no files to get data from!")
                     pass
         
-        self.input_data["x_star"] = x_star
-        self.input_data["y_star"] = y_star
-        self.input_data["t_star"] = t_star
-        self.input_data["C_star"] = C_star
-        self.input_data["U_star"] = U_star
-        self.input_data["V_star"] = V_star
-        self.input_data["P_star"] = P_star
+        self.Inputs.x_star = x_star
+        self.Inputs.y_star = y_star
+        self.Inputs.t_star = t_star
+        self.Inputs.C_star = C_star
+        self.Inputs.U_star = U_star
+        self.Inputs.V_star = V_star
+        self.Inputs.P_star = P_star
+    #rearrange data
+    def rearrangeData(self):
 
-    
+        if(self.input_data != None):
+            T = (self.input_data["t_star"]).shape[0]
+            N = (self.input_data["x_star"]).shape[0]
+
+            self.rearranged_inputs["T_star"] = np.tile(self.input_data["t_star"], (1,N)).T
+            self.rearranged_inputs["X_star"] = np.tile(self.input_data["x_star"], (1,T))
+            self.rearranged_inputs["Y_star"] = np.tile(self.input_data["y_star"], (1,T))
+
+            # Training Data
+            T_data = T
+            N_data = N
+            idx_t = np.concatenate([np.array([0]), np.random.choice(T-2, T_data-2, replace=False)+1, np.array([T-1])] )
+            idx_x = np.random.choice(N, N_data, replace=False)
+            t_data = self.rearranged_inputs["T_star"][:, idx_t][idx_x,:].flatten()[:,None]
+            x_data = self.rearranged_inputs["X_star"][:, idx_t][idx_x,:].flatten()[:,None]
+            y_data = self.rearranged_inputs["Y_star"][idx_x,:].flatten()[:,None]
+            c_data = self.input_data["C_Star"][:, idx_t][idx_x,:].flatten()[:,None]
 
     # creates a mesh template for the Pipeline object
     # pass in the array of filenames to pick one and use the coordinates and cell format as a template for an output mesh
@@ -315,7 +377,7 @@ class Pipeline:
 
 
     def mat2vtu(self, mesh_template=None, predictions: dict=None):
-        if (mesh_template == None and results == None):
+        if (mesh_template == None and predictions == None):
             if (self.mesh_temp == None): raise IOError("No mesh template to use! Try creating one first!")
             points = self.mesh_temp.points
             output = pd.DataFrame(float)
@@ -336,10 +398,146 @@ class Pipeline:
                     point_data
                 )
                 mesh.write("_"+str(i)+".vtu")
-        # if neither mesh_template nor mat_data exist,
+        # if mesh_template and predictions are provided
         else:
             return
 
+class HFM:
+    # notational conventions
+    # _tf: placeholders for input/output data and points used to regress the equations
+    # _pred: output of neural network
+    # _eqns: points used to regress the equations
+    # _data: input-output data
+    # _star: preditions
+    
+    def __init__(hfm, t_data, x_data, y_data, c_data,
+                       t_eqns, x_eqns, y_eqns,
+                       layers, batch_size,
+                       Pec, Rey):
+        
+        # specs
+        hfm.layers = layers
+        hfm.batch_size = batch_size
+        
+        # flow properties
+        hfm.Pec = Pec
+        hfm.Rey = Rey
+        
+        # data
+        [hfm.t_data, hfm.x_data, hfm.y_data, hfm.c_data] = [t_data, x_data, y_data, c_data]
+        [hfm.t_eqns, hfm.x_eqns, hfm.y_eqns] = [t_eqns, x_eqns, y_eqns]
+        
+        # placeholders
+        [hfm.t_data_tf, hfm.x_data_tf, hfm.y_data_tf, hfm.c_data_tf] = [tf.placeholder(tf.float32, shape=[None, 1]) for _ in range(4)]
+        [hfm.t_eqns_tf, hfm.x_eqns_tf, hfm.y_eqns_tf] = [tf.placeholder(tf.float32, shape=[None, 1]) for _ in range(3)]
+        
+        # physics "uninformed" neural networks
+        hfm.net_cuvp = neural_net(hfm.t_data, hfm.x_data, hfm.y_data, layers = hfm.layers)
+        
+        [hfm.c_data_pred,
+         hfm.u_data_pred,
+         hfm.v_data_pred,
+         hfm.p_data_pred] = hfm.net_cuvp(hfm.t_data_tf,
+                                           hfm.x_data_tf,
+                                           hfm.y_data_tf)
+         
+        # physics "informed" neural networks
+        [hfm.c_eqns_pred,
+         hfm.u_eqns_pred,
+         hfm.v_eqns_pred,
+         hfm.p_eqns_pred] = hfm.net_cuvp(hfm.t_eqns_tf,
+                                           hfm.x_eqns_tf,
+                                           hfm.y_eqns_tf)
+        
+        [hfm.e1_eqns_pred,
+         hfm.e2_eqns_pred,
+         hfm.e3_eqns_pred,
+         hfm.e4_eqns_pred] = Navier_Stokes_2D(hfm.c_eqns_pred,
+                                               hfm.u_eqns_pred,
+                                               hfm.v_eqns_pred,
+                                               hfm.p_eqns_pred,
+                                               hfm.t_eqns_tf,
+                                               hfm.x_eqns_tf,
+                                               hfm.y_eqns_tf,
+                                               hfm.Pec,
+                                               hfm.Rey)
+        
+        # loss
+        hfm.loss = mean_squared_error(hfm.c_data_pred, hfm.c_data_tf) + \
+                    mean_squared_error(hfm.e1_eqns_pred, 0.0) + \
+                    mean_squared_error(hfm.e2_eqns_pred, 0.0) + \
+                    mean_squared_error(hfm.e3_eqns_pred, 0.0) + \
+                    mean_squared_error(hfm.e4_eqns_pred, 0.0)
+        
+        # optimizers
+        hfm.learning_rate = tf.placeholder(tf.float32, shape=[])
+        hfm.optimizer = tf.train.AdamOptimizer(learning_rate = hfm.learning_rate)
+        hfm.train_op = hfm.optimizer.minimize(hfm.loss)
+        
+        hfm.sess = tf_session()
+        
+    def train(hfm, total_time, learning_rate):
+        
+        N_data = hfm.t_data.shape[0]
+        N_eqns = hfm.t_eqns.shape[0]
+        
+        start_time = time.time()
+        running_time = 0
+        it = 0
+        while running_time < total_time:
+            
+            idx_data = np.random.choice(N_data, min(hfm.batch_size, N_data))
+            idx_eqns = np.random.choice(N_eqns, hfm.batch_size)
+            
+            (t_data_batch,
+             x_data_batch,
+             y_data_batch,
+             c_data_batch) = (hfm.t_data[idx_data,:],
+                              hfm.x_data[idx_data,:],
+                              hfm.y_data[idx_data,:],
+                              hfm.c_data[idx_data,:])
+
+            (t_eqns_batch,
+             x_eqns_batch,
+             y_eqns_batch) = (hfm.t_eqns[idx_eqns,:],
+                              hfm.x_eqns[idx_eqns,:],
+                              hfm.y_eqns[idx_eqns,:])
+
+
+            tf_dict = {hfm.t_data_tf: t_data_batch,
+                       hfm.x_data_tf: x_data_batch,
+                       hfm.y_data_tf: y_data_batch,
+                       hfm.c_data_tf: c_data_batch,
+                       hfm.t_eqns_tf: t_eqns_batch,
+                       hfm.x_eqns_tf: x_eqns_batch,
+                       hfm.y_eqns_tf: y_eqns_batch,
+                       hfm.learning_rate: learning_rate}
+            
+            hfm.sess.run([hfm.train_op], tf_dict)
+            
+            # Print
+            if it % 10 == 0:
+                elapsed = time.time() - start_time
+                running_time += elapsed/3600.0
+                [loss_value,
+                 learning_rate_value] = hfm.sess.run([hfm.loss,
+                                                       hfm.learning_rate], tf_dict)
+                print('It: %d, Loss: %.3e, Time: %.2fs, Running Time: %.2fh, Learnsing Rate: %.1e'
+                      %(it, loss_value, elapsed, running_time, learning_rate_value))
+                sys.stdout.flush()
+                start_time = time.time()
+            it += 1
+    
+    def predict(hfm, t_star, x_star, y_star):
+        
+        tf_dict = {hfm.t_data_tf: t_star, hfm.x_data_tf: x_star, hfm.y_data_tf: y_star}
+        
+        c_star = hfm.sess.run(hfm.c_data_pred, tf_dict)
+        u_star = hfm.sess.run(hfm.u_data_pred, tf_dict)
+        v_star = hfm.sess.run(hfm.v_data_pred, tf_dict)
+        p_star = hfm.sess.run(hfm.p_data_pred, tf_dict)
+        
+        return c_star, u_star, v_star, p_star
 
 def main():
     try:
@@ -349,7 +547,7 @@ def main():
 
     mesh_template = meshio.read("misc/mesh_template.vtu")
     results_mat = scipy.io.loadmat("training_120k_results_26_07_2021.mat")
-    mat2vtu(mesh_template, results_mat)
+    #mat2vtu(mesh_template, results_mat)
     # fNames = parse_args()["fileNames"]
     
     # if fNames:

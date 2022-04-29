c43157fa276ac5bea151da134f558e527736e5b8
amorx1 <akshay.morx@gmail.com>
2022-04-29 18:21:39 +0100

    clean

--- Source/Run.py
+++ Source/Run.py
@@ -60,53 +60,7 @@ def main():
     pipeline.createMeshTemplate()
     pipeline.writeVTU()
 
-    # print("Done")
+    print("Done")
     
-    # # Training
-    # model = HFM(t_data, x_data, y_data, c_data,
-    #             t_eqns, x_eqns, y_eqns,
-    #             layers, batch_size,
-    #             Pec = 100, Rey = 100)
-        
-    # model.train(total_time = 0.05, learning_rate=1e-3)
-    
-    # ################# Save Data ###########################
-    
-    # C_pred = 0*C_star
-    # U_pred = 0*U_star
-    # V_pred = 0*V_star
-    # P_pred = 0*P_star
-    # for snap in range(0,t_star.shape[0]):
-    #     t_test = T_star[:,snap:snap+1]
-    #     x_test = X_star[:,snap:snap+1]
-    #     y_test = Y_star[:,snap:snap+1]
-        
-    #     c_test = C_star[:,snap:snap+1]
-    #     u_test = U_star[:,snap:snap+1]
-    #     v_test = V_star[:,snap:snap+1]
-    #     p_test = P_star[:,snap:snap+1]
-    
-    #     # Prediction
-    #     c_pred, u_pred, v_pred, p_pred = model.predict(t_test, x_test, y_test)
-        
-    #     C_pred[:,snap:snap+1] = c_pred
-    #     U_pred[:,snap:snap+1] = u_pred
-    #     V_pred[:,snap:snap+1] = v_pred
-    #     P_pred[:,snap:snap+1] = p_pred
-    
-    #     # Error
-    #     error_c = relative_error(c_pred, c_test)
-    #     error_u = relative_error(u_pred, u_test)
-    #     error_v = relative_error(v_pred, v_test)
-    #     error_p = relative_error(p_pred - np.mean(p_pred), p_test - np.mean(p_test))
-    
-    #     print('Error c: %e' % (error_c))
-    #     print('Error u: %e' % (error_u))
-    #     print('Error v: %e' % (error_v))
-    #     print('Error p: %e' % (error_p))
-    
-    # scipy.io.savemat('../Results/Cylinder2D_flower_results_%s.mat' %(time.strftime('%d_%m_%Y')),
-    #                 {'C_pred':C_pred, 'U_pred':U_pred, 'V_pred':V_pred, 'P_pred':P_pred})
- 
 if __name__ == "__main__":
     main()
